fbc9dd6c7cc9ba2951c6d21ceb21d7d031e47735
Adriel Puello <adrielpuello@gmail.com>
2023-09-26 19:24:48 -0400

    A bunch of css updates, beginning work on the about section

--- my-ts-react-app/src/components/About/About.tsx
+++ my-ts-react-app/src/components/About/About.tsx
@@ -1,11 +1,46 @@
 import React from "react";
 
+import guitar from './Images/ajcassandraguitar.jpg'
+import lake from './Images/ajcassandralake.jpg'
+import calzone from './Images/calzone.jpeg'
+import prettyplace from './Images/prettyplace.jpg'
+import wakeboard from './Images/wakeboarding.jpg'
+
 const About: React.FC = () => {
 
     return (
-        <div className="main-text">
-            <h1>About</h1>
-        </div>
+        <>
+            <div className="main-text">
+                    <h1>Mi Vida 🌏</h1>
+                    <h2>Who am I?</h2>
+                    <p>Well I'm so glad you asked! I was born in the Dominican Republic 🏝️, and moved to the states when I was 7 years old. Since I was a child, I've loved adventures and physical activity of any kind. In my day to day life I enjoy hiking, singing and songwriting, playing videogames, and spending time with my loved ones.</p>
+                    <h2>My Goals</h2>
+                    <p>Ultimately, I strive to impact the world in as many positive ways as possible. 
+                    That means regardless of the field I work in, and more importantly in spite of all 
+                    the adversity life has to offer, doing everything I can to make a difference, no matter how small.
+                    My ultimate passion is music, and I someday hope to be able to make a living off of my artistry. 
+                    At the same time, I love the challenge the field of software engineering has to offer. 
+                    I am all for learning as much as possible, and mastering my craft. On a more personal note, 
+                    I hope to someday estbalish a non-profit organization that can provide dependable, trustworthy men
+                    to step into the lives of children, specifically young men, who are growing up in our country without a dad.</p>
+                </div>
+            <div className="about-container">
+                <div className="image-container">
+                    <div className="image-container-one">
+                        <img className="about-image-1" src={guitar} alt='guitar'></img>
+                        <img className="about-image-1" src={lake} alt='lake'></img>
+                    </div>
+                    <div className="image-container-two">
+                        <img className="about-image-2" src={prettyplace} alt='prettyplace'></img>
+                    </div>
+                    <div className="image-container-three">
+                        <img className="about-image-3" src={calzone} alt='calzone'></img>
+                        <img className="about-image-3" src={wakeboard} alt='wakeboard'></img>
+                    </div>      
+                </div> 
+            </div>
+        </>
+        
     );
 };
 
--- my-ts-react-app/src/components/Home/Home.tsx
+++ my-ts-react-app/src/components/Home/Home.tsx
@@ -4,18 +4,19 @@ const Home: React.FC = () => {
 
     return (
         <div className="main-text">
-            <h1>Adriel Puello</h1>
-            <p>Hello! I'm a passionate software engineer with a diverse skill set that spans the realms of web development. My journey through the world of technology has led me to become proficient in several key areas, and I thrive at the intersection of innovation and user-centric design.
+            <h1>Hey there, I'm AJ 👋🏽</h1>
+                <p>Hello! I'm a passionate software engineer with a diverse skill set that spans the realms of web development. My journey through the world of technology has led me to become proficient in several key areas, and I thrive at the intersection of innovation and user-centric design.
 
-My primary expertise lies in React, where I've honed my skills in crafting responsive, interactive, and high-performance user interfaces. With a strong foundation in TypeScript, I ensure robust code quality and maintainability while leveraging the power of static typing.
+                My primary expertise lies in React, where I've honed my skills in crafting responsive, interactive, and high-performance user interfaces. With a strong foundation in JavaScript, I ensure robust code quality and maintainability while leveraging the power of static typing.
 
-In the backend realm, I've found my stride with Ruby on Rails, an elegant and powerful framework that enables rapid development of scalable and maintainable web applications. Building robust APIs, integrating databases, and crafting RESTful endpoints are all in a day's work for me.
+                In the backend realm, I've found my stride with Ruby on Rails, an elegant and powerful framework that enables rapid development of scalable and maintainable web applications. Building robust APIs, integrating databases, and crafting RESTful endpoints are all in a day's work for me.
 
-And when it comes to aesthetics and user experience, I bring a keen eye for design and a mastery of CSS to the table. I take pride in crafting beautiful and intuitive user interfaces, ensuring that every pixel is in its right place.
+                And when it comes to aesthetics and user experience, I bring a keen eye for design and a mastery of CSS to the table. I take pride in crafting beautiful and intuitive user interfaces, ensuring that every pixel is in its right place.
 
-My journey in software engineering has been driven by a genuine passion for creating technology that makes a difference. I'm eager to tackle new challenges, collaborate with creative teams, and build solutions that empower and delight users.
+                My journey in software engineering has been driven by a genuine passion for creating technology that makes a difference. I'm eager to tackle new challenges, collaborate with creative teams, and build solutions that empower and delight users.
 
-Let's create something remarkable together! 💻🚀</p>
+                Let's create something remarkable together! 💻🚀</p>
+            <div className="footer"></div>
         </div>
     );
 };
