f81011d8187a336cf4ff2c8dafb2a4ab73e17ff6
Adriel Puello <adrielpuello@gmail.com>
2023-09-27 11:20:37 -0400

    creating a better user experience for mobile devices

--- src/App.tsx
+++ src/App.tsx
@@ -20,10 +20,6 @@ const App: React.FC = () => {
 
   return (
     <div className="main-container">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
-      </head>
       <div className={`App ${isDarkModeEnabled ? 'dark-mode-app' : ''}`}>
         <Navbar activePage={activePage} 
         onNavItemClick={handleNavItemClick} 
